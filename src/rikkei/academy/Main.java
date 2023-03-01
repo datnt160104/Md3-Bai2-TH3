a28660d2a8d0b0a118d7b6383cf0e32eec23e0d2
Đạt <dat16010425@gmail.com>
2023-03-02 00:39:26 +0700

    th2.3

---
+++ src/rikkei/academy/Main.java
@@ -0,0 +1,28 @@
+package rikkei.academy;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int b = scanner.nextInt();
+
+        int gcd = findGCD(a, b);
+        System.out.println("GCD of " + a + " and " + b + " is " + gcd);
+
+        scanner.close();
+    }
+
+    public static int findGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return findGCD(b, a % b);
+        }
+    }
+}
